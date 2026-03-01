87d00f8bbf777ce26a0e951bc1ff4a8480631849
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-03-02 00:26:02 +0800

    adding completed my-express-app activity

---
+++ my-express-app/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.post('/submit', (req, res) => {
+  const data = req.body;
+  res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+let items = [];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const newItem = req.body.item;
+  items.push(newItem);
+  res.json(items);
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
